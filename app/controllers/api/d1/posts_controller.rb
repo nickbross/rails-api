2b03cd6ffbfeeed0c8d1f23929773305f3aa100f
Nicholas Bross <nickrbross@gmail.com>
2020-12-09 17:38:40 -0800

    added capability for users to have multiple posts

--- app/controllers/api/d1/posts_controller.rb
+++ app/controllers/api/d1/posts_controller.rb
@@ -1,15 +1,17 @@
 class Api::D1::PostsController < ApplicationController
     def index
-        @posts = Post.all()
+        @posts = Post.where(user_id: params[:user_id])
         render json: @posts
     end
 
     def show
+        @post = Post.find_by(user_id: params[:user_id], id: params[:id])
         render json: @post
     end
 
     def new
-            
+        @post = Post.new({fact: "bullshit", likes: 123, user_id: 1})        
+        @post.save!
     end
 end
 
